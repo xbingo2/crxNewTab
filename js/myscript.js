196bb8aea819c01bf1aaf2a8da6023208ae1514c
xubin <xubin@wisdytech.cn>
2020-09-14 17:14:51 +0800

    添加天气

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 function updateClock() {
-	$(".clock")[0].innerHTML = moment().format('HH:mm:ss');
+	$(".clock")[0].innerHTML = moment().format('HH:mm:ss dddd');
 }
 updateClock();
 setInterval(updateClock, 1000);
