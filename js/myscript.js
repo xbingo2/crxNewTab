c989e96ae42170b8bf2664f94265d68eb88b2026
xubin <xubin@wisdytech.cn>
2021-12-24 19:19:29 +0800

    '配置页修改，主页添加设置图标'

--- js/myscript.js
+++ js/myscript.js
@@ -3,3 +3,22 @@ function updateClock() {
 }
 updateClock();
 setInterval(updateClock, 1000);
+
+//获取天气信息
+$.ajax({
+        type: 'GET',
+        url: 'https://www.yiketianqi.com/free/day?appid=23035354&appsecret=8YvlPNrz',
+        dataType: 'JSON',
+        error: function () {
+            alert('网络错误');
+        },
+        success: function (res) {
+            $('#box').append(res.city + '  ' + res.wea + '  ' + res.tem + "°C" + '  (' + res.tem_night + '~' + res.tem_day + '°C)');
+        }
+    });
+  
+$(function() {
+	$( "#draggable" ).draggable({
+    containment: $('body')
+});
+});
--- js/options.js
+++ js/options.js
@@ -1,18 +1,88 @@
 document.addEventListener('DOMContentLoaded', function() {
-	chrome.storage.sync.get(_CONFIG, function(items) {
-		document.getElementById('config').value = JSON.stringify(items);
-	});
+	setConfigTable();
 });
 
-document.getElementById('save').addEventListener('click', function() {
-	var config = document.getElementById('config').value;
-	try{
-		var configObj = JSON.parse(config);
-		chrome.storage.sync.set(configObj, function() {
-			document.getElementById('msg').text='保存成功！';
+function setConfigTable(){
+	chrome.storage.sync.get(_CONFIG, function(items) {
+		var array = items.mangabz;
+		var trStr = "";
+		for (var i = 0; i < array.length; i ++) {
+			trStr += ('<tr><td>' + (i+1) + '</td>' 
+						 +'<td>' + array[i].name + '</td>'
+						 +'<td>' + array[i].url + '</td>'
+						 +'<td>' + array[i].selector + '</td>'
+						 +'<td>' + (array[i].type ? array[i].type : '') + '</td>'
+						 +'<td><button type="button" class="btn btn-primary btn-sm updateConfig" >修改</button>&nbsp;&nbsp;&nbsp;&nbsp;'
+						 +'<button type="button" class="btn btn-danger btn-sm deleteConfig">删除</button></td><tr>');
+		}
+		$("#tb").empty();
+		$("#tb").append(trStr);
+	});
+}
+
+//新增弹窗
+$("#addBtn").click(function(){
+	$('#dialog-title').text('新增');
+	$('#dialog').modal('show');
+})
+
+//更新弹窗
+$("table").on('click', '.updateConfig', function() {
+	var tdArray = $(this).parent().parent().children();
+	var index = $(tdArray[0]).text() - 1;
+	$('#dialog-title').text('修改');
+	$('#dialog-name').val($(tdArray[1]).text());
+	$('#dialog-url').val($(tdArray[2]).text());
+	$('#dialog-selector').val($(tdArray[3]).text());
+	$('#dialog-type').val($(tdArray[4]).text());
+	$('#dialog-index').val(index);
+	$('#dialog').modal('show');
+})
+
+//删除
+$("table").on('click', '.deleteConfig', function() {
+	var tdArray = $(this).parent().parent().children();
+	var index = $(tdArray[0]).text() - 1;
+	chrome.storage.sync.get(_CONFIG, function(items) {
+		var array = [];
+		for (var i = 0; i < items.mangabz.length; i ++) {
+			if (i != index) {
+				array.push(items.mangabz[i]);
+			}
+		}
+		chrome.storage.sync.set({"mangabz":array}, function() {
+			setConfigTable();
 		});
-	}catch(e){
-		document.getElementById('msg').text='保存失败！';
+	})
+})
+
+//保存
+$("#dialog-save").click(function() {
+	var title = $('#dialog-title').text();
+	var index = $('#dialog-index').val();
+	var obj = {"name":$('#dialog-name').val(),"url":$('#dialog-url').val(),"selector":$('#dialog-selector').val(),"type":$('#dialog-type').val()};
+	if (title == '修改') {
+		chrome.storage.sync.get(_CONFIG, function(items) {
+			var array = items.mangabz;
+			array[index] = obj
+			chrome.storage.sync.set({"mangabz":array}, function() {
+				$('#dialog').modal('hide');
+				setConfigTable();
+			});
+		})
+	}
+	else if (title == '新增'){
+		chrome.storage.sync.get(_CONFIG, function(items) {
+			var array = items.mangabz;
+			array.push(obj)
+			chrome.storage.sync.set({"mangabz":array}, function() {
+				$('#dialog').modal('hide');
+				setConfigTable();
+			});
+		})
 	}
 	
-});
+})
+
+
+
