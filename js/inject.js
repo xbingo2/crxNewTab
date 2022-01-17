2678362ed53a20cb91055c1647afea86786a9052
xubin <xubin@wisdytech.cn>
2022-01-17 11:36:50 +0800

    csdn  //优化登陆后复制 //移除“登陆后复制”按钮 //移除readmore按钮，并显示全文  //去除复制后的copyright小尾巴

--- js/inject.js
+++ js/inject.js
@@ -21,6 +21,25 @@
 			onmousedown = null
 		}
 	};
+	if(window.location.origin.indexOf("blog.csdn.net") > -1) {
+		//优化登陆后复制
+	    $('code').css({'user-select':'unset'});
+	    $('#content_views pre').css({'user-select':'unset'});
+	 
+	    //移除“登陆后复制”按钮
+	     $('.hljs-button').remove();
+	    //移除readmore按钮，并显示全文
+	    $('.hide-article-box').remove();
+	    $('.article_content').css({'height':'initial'});
+	 
+	    //去除复制后的copyright小尾巴
+	    document.querySelectorAll('*').forEach(item => {
+		    item.oncopy = function(e) {
+		        e.stopPropagation();
+		    }
+		});
+   }
+	
 })()
 
 // 根据规则隐藏页面元素
